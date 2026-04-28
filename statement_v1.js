d244c484fbf0387c46fedf4dedf6cde312ad66c8
Kai Laitinen <klaitinen@cs.linfield.edu>
2026-04-28 14:06:09 -0700

    runs again and exported switch function and credit function

--- statement_v1.js
+++ statement_v1.js
@@ -68,6 +68,9 @@
 //     ]
 // }
 
+invoice = require('./invoice.json')
+plays = require('./plays.json')
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -76,30 +79,9 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
         let thisAmount = 0;
-        switch (play.type) {
-            case 'tragedy':
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case 'comedy':
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        thisAmount = switchFunction(play, thisAmount, perf);
         // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-        // print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience
-            } seats)\n`;
+        ({ volumeCredits, result } = convertCredits(volumeCredits, perf, play, result, format, thisAmount));
         totalAmount += thisAmount;
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
@@ -112,6 +94,36 @@ function statement(invoice, plays) {
 let stmt = statement(invoice, plays)
 console.log(stmt)
 
+
+function convertCredits(volumeCredits, perf, play, result, format, thisAmount) {
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    // print line for this order
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    return { volumeCredits, result };
+}
+
+function switchFunction(play, thisAmount, perf) {
+    switch (play.type) {
+        case 'tragedy':
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case 'comedy':
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+}
 
 function formatCurrency() {
     return new Intl.NumberFormat('en-US', {
